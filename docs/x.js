098cba381e27e96893187e964d9cc5b2435c6199
educationvirus1-create <educationvirus1@gmail.com>
2025-12-10 18:39:02 +0000

    Add docs/x.js loader for short bookmark

---
+++ docs/x.js
@@ -0,0 +1,6 @@
+(function(){
+  var s=document.createElement('script');
+  s.src='https://cdn.jsdelivr.net/gh/educationvirus1-create/bcp@adb6d77/bcp.js';
+  s.async=true;
+  document.head.appendChild(s);
+})();
